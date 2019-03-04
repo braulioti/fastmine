d56cf3e8f2c7b7818caa6caa7396d78cb7b717ab
Bráulio Figueiredo <braulio@braulioti.com.br>
2019-03-04 15:24:20 -0300

    #23 Created a first model to synchronize model from Redmine to RedTrench

---
+++ src/node/models/projects.model.ts
@@ -0,0 +1,46 @@
+import {Column, Model, Table} from 'sequelize-typescript';
+
+@Table({tableName: 'projects'})
+export class Projects extends Model<Projects> {
+    @Column
+    name: string;
+
+    @Column
+    description: string;
+
+    @Column
+    homepage: string;
+
+    @Column({field: 'is_public'})
+    isPublic: number;
+
+    @Column({field: 'parent_id'})
+    parentId: number;
+
+    @Column({field: 'created_on'})
+    createdOn: Date;
+
+    @Column({field: 'updated_on'})
+    updatedOn: Date;
+
+    @Column
+    identifier: string;
+
+    @Column
+    status: number;
+
+    @Column
+    lft: number;
+
+    @Column
+    rgt: number;
+
+    @Column({field: 'inherit_members'})
+    inheritMembers: number;
+
+    @Column({field: 'default_version_id'})
+    defaultVersionId: number;
+
+    @Column({field: 'default_assigned_to_id'})
+    defaultAssignedToId: number;
+}
